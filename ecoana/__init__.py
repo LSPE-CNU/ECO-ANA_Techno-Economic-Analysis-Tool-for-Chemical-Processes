3c3f2deb4953c9cf0b1a79757bd789c74a63638f
LSPE-CNU <lspecnu@gmail.com>
2026-09-11 18:31:13 +0900

    Delete ecoana directory

--- ecoana/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-ECO-ANA: Techno-Economic Analysis Tool for Chemical Processes
-This was developed by LSPE, the Laboratory for Sustainable Process Engineering from Chungnam national university (CNU).
-"""
